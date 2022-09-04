8e34919e31059fcbb9020f59da752853210306da
Marcelo Barbosa <marcelo.castilho2707@gmail.com>
2022-09-04 20:19:03 -0300

    Adding Screens

--- src/components/Navigator/index.js
+++ src/components/Navigator/index.js
@@ -1,6 +1,8 @@
 import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
-import Home from "../Home"
+import Calendar from "../../screens/Calendar"
+import Home from "../../screens/Home"
+import Standing from "../../screens/Standing"
 
 const Stack = createNativeStackNavigator()
 
@@ -9,6 +11,8 @@ const Navigator = () => {
         <NavigationContainer>
             <Stack.Navigator>
                 <Stack.Screen name="HomeScreeen" component={Home}/>
+                <Stack.Screen name="StandingScreeen" component={Standing}/>
+                <Stack.Screen name="CalendarScreeen" component={Calendar}/>
             </Stack.Navigator>
         </NavigationContainer>
     )
---
+++ src/screens/Standing/index.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native"
+
+const Standing = () => {
+     return(
+        <View>
+            <Text>
+                StandingScreen
+            </Text>
+        </View>
+     )
+}
+
+export default Standing
